ac41ae033e60933b4586d4911b60b30e86083f52
SAID <ismael.saidpro@protonmail.com>
2024-05-16 12:32:45 +0400

    ProductFixtures.php: Ajout de données supplémentaires dans la base de données

--- video-drive-back/src/DataFixtures/ProductFixtures.php
+++ video-drive-back/src/DataFixtures/ProductFixtures.php
@@ -40,7 +40,7 @@ class ProductFixtures extends Fixture
                 'name' => 'FIFA 21',
                 'description' => 'Jeu de foot',
                 'price' => 59.99,
-                'brand' => 'EA Sports',
+                'brand' => 'Sony',
                 'category' => 'Jeu',
                 'picture' => 'assets/fifa21.png'
             ],
@@ -48,9 +48,57 @@ class ProductFixtures extends Fixture
                 'name' => 'Call of Duty',
                 'description' => 'Jeu de guerre',
                 'price' => 59.99,
-                'brand' => 'Activision',
+                'brand' => 'Sony',
                 'category' => 'Jeu',
                 'picture' => 'assets/cod.png'
+            ],
+            [
+                'name' => 'Monster hunter world',
+                'description' => 'Jeu de guerre',
+                'price' => 59.99,
+                'brand' => 'Microsoft',
+                'category' => 'Jeu',
+                'picture' => 'assets/mhwwordxbox.jpeg'
+            ],
+            [
+                'name' => 'Manette ps5',
+                'description' => 'Manette noire',
+                'price' => 59.99,
+                'brand' => 'Sony',
+                'category' => 'Accessoire',
+                'picture' => 'assets/manetteps5.jpg'
+            ],
+            [
+                'name' => 'FC 24',
+                'description' => 'Jeu de foot',
+                'price' => 59.99,
+                'brand' => 'Sony',
+                'category' => 'Jeu',
+                'picture' => 'assets/81swenNfj-L._AC_SL1500_.jpg'
+            ],
+            [
+                'name' => 'GTA V',
+                'description' => 'Jeu de guerre',
+                'price' => 59.99,
+                'brand' => 'Sony',
+                'category' => 'Jeu',
+                'picture' => 'assets/81n7+V7GZcL._AC_SL1500_.jpg'
+            ],
+            [
+                'name' => 'Red Dead Redemption 2',
+                'description' => 'Jeu de guerre',
+                'price' => 59.99,
+                'brand' => 'Sony',
+                'category' => 'Jeu',
+                'picture' => 'assets/71lUrseKOjL._AC_SL1000_.jpg'
+            ],
+            [
+                'name' => 'The last of us 2',
+                'description' => 'Jeu de guerre',
+                'price' => 59.99,
+                'brand' => 'Sony',
+                'category' => 'Jeu',
+                'picture' => 'assets/81hEsg04h0L._AC_SL1500_.jpg'
             ]
         ];
 
