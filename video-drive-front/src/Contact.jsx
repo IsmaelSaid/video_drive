8a5e60e069a5e7f1b95de541973e667997e3023b
SAID <ismael.saidpro@protonmail.com>
2024-05-16 12:34:04 +0400

    Contact.jsx, .env: Ajout de la possibilité d'envoyer des mails via mailjs

--- video-drive-front/src/Contact.jsx
+++ video-drive-front/src/Contact.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {ButtonAppBar} from "./AppBar.jsx";
-import {FormControl, TextField} from "@mui/material";
+import {Alert, AlertTitle, Collapse, FormControl, IconButton, TextField} from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import LocationOnIcon from '@mui/icons-material/LocationOn';
@@ -8,14 +8,40 @@ import PhoneEnabledIcon from '@mui/icons-material/PhoneEnabled';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import {Mail} from "@mui/icons-material";
+import emailjs from '@emailjs/browser';
+import Typography from "@mui/material/Typography";
+import CloseIcon from '@mui/icons-material/Close';
+
 
 export function Contact() {
+    const form = useRef();
+    const [alert, setAlert] = useState(true);
+    const [open, setOpen] = useState(false);
+
+    const sendEmail = (e) => {
+        console.log('trigger')
+        e.preventDefault();
+
+        emailjs
+            .sendForm(import.meta.env.VITE_SERVICE_EMAILJS, import.meta.env.VITE_SERVICE_TEMPLATE, form.current, {
+                publicKey: import.meta.env.VITE_PUBLIC_KEY_EMAILJS,
+            })
+            .then(
+                () => {
+                    console.info('SUCCESS!');
+                    setOpen(true)
+                },
+                (error) => {
+                    console.info('FAILED...', error.text);
+                },
+            );
+    };
+
     return (
         <div style={{height: "100vh", display: "flex", flexDirection: 'column', justifyContent: "center"}}>
             <ButtonAppBar></ButtonAppBar>
             <div style={{
                 height: '100%',
-                border: '1px solid black',
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center"
@@ -30,25 +56,27 @@ export function Contact() {
                     backgroundColor: "white",
                     padding: "20px",
                 }}>
-                    <h1>Nous envoyer un message</h1>
-                    <div style={{ height : "100%", display : "flex", justifyContent : "center", flexDirection : "column"}}>
-                        <form style={{display: "flex", flexDirection: "column"}}>
+                    <Typography variant={'h5'}>
+                        Nous envoyer un message
+                    </Typography>
+                    <div style={{height: "100%", display: "flex", justifyContent: "center", flexDirection: "column"}}>
+                        <form ref={form} style={{display: "flex", flexDirection: "column"}} onSubmit={sendEmail}>
                             <Box>
                                 <FormControl style={{marginBottom: '20px'}} fullWidth>
-                                    <TextField id="standard-basic" label="nom" variant="filled"/>
+                                    <TextField required={true} id="standard-basic" label="Nom" name='user_name'
+                                               variant="filled"/>
                                 </FormControl>
 
 
                                 <FormControl style={{marginBottom: '20px'}} fullWidth>
-                                    <TextField id="standard-basic" label="prenom" variant="filled"/>
-                                </FormControl>
-
-                                <FormControl style={{marginBottom: '20px'}} fullWidth>
-                                    <TextField id="standard-basic" label="email" variant="filled"/>
+                                    <TextField required={true} id="standard-basic" label="Email" name='user_email'
+                                               variant="filled"/>
                                 </FormControl>
 
                                 <FormControl style={{marginBottom: '20px'}} fullWidth>
                                     <TextField
+                                        required={true}
+                                        name='message'
                                         id="filled-multiline-static"
                                         label="Votre message"
                                         multiline
@@ -57,11 +85,11 @@ export function Contact() {
                                     />
                                 </FormControl>
 
-
-                                <Button>
+                                <Button type={"submit"}>
                                     Envoyer
 
                                 </Button>
+
                             </Box>
                         </form>
 
@@ -76,29 +104,40 @@ export function Contact() {
                     flexDirection: "column",
                     justifyContent: "center",
                     alignItems: "center",
-                    backgroundColor: "lightcyan",
                     padding: "20px",
                 }}>
 
-                    <h1>Nous contacter</h1>
-                    <div style={{height: "100%", display : "flex"}}>
-                        <ul style={{listStyleType: "none", display : "flex", justifyContent : "center", flexDirection:"column"}}>
+                    <Typography variant={'h5'}>
+                        Nous contacter
+                    </Typography>
+                    <div style={{height: "100%", display: "flex"}}>
+                        <ul style={{
+                            listStyleType: "none",
+                            display: "flex",
+                            justifyContent: "center",
+                            flexDirection: "column"
+                        }}>
 
-                            <li style={{display: "flex", alignItems: "center", marginBottom : "20px"}}>
+                            <li style={{display: "flex", alignItems: "center", marginBottom: "20px"}}>
                                 <LocationOnIcon/>
-                                <span>3 rue de la République, 97417 Saint Denis, La Réunion</span>
+                                <Typography>
+                                    3 Rue de la République, 97417 Saint Denis, La Réunion
+                                </Typography>
                             </li>
-                            <li style={{display: "flex", alignItems: "center", marginBottom : "20px"}}>
+                            <li style={{display: "flex", alignItems: "center", marginBottom: "20px"}}>
                                 <PhoneEnabledIcon/>
-                                <span>06934123456</span>
+                                <Typography>
+                                    069 31 23 45 67
+                                </Typography>
                             </li>
-                            <li style={{display: "flex", alignItems: "center", marginBottom : "20px"}}>
+                            <li style={{display: "flex", alignItems: "center", marginBottom: "20px"}}>
                                 <Mail/>
-                                <span>admin@videodrive.fr</span>
+
+                                <Typography>admin@videodrive.fr</Typography>
                             </li>
-                            <li style={{display: "flex", alignItems: "center", marginBottom : "20px"}}>
+                            <li style={{display: "flex", alignItems: "center", marginBottom: "20px"}}>
                                 <LinkedInIcon/>
-                                <span>/linkedin/vd</span>
+                                <Typography>/linkedin/vd</Typography>
                             </li>
 
                         </ul>
@@ -110,6 +149,28 @@ export function Contact() {
 
 
             </div>
+            {  <Box sx={{ width: '100%', position : 'absolute', bottom : '0px'}}>
+                <Collapse in={open}>
+                    <Alert
+                        action={
+                            <IconButton
+                                aria-label="close"
+                                color="inherit"
+                                size="small"
+                                onClick={() => {
+                                    setOpen(false);
+                                }}
+                            >
+                                <CloseIcon fontSize="inherit" />
+                            </IconButton>
+                        }
+                        sx={{ mb: 2 }}
+                    >
+                        Votre message a été envoyé avec succès
+                    </Alert>
+                </Collapse>
+            </Box>}
+
 
         </div>
     );
